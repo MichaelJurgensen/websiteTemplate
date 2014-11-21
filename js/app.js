9ce5ab289f07f1cea986d756b8669829948cce39
MichaelJurgensen <ryeprogrammer@gmail.com>
2014-11-21 17:25:11 -0500

    Fixed sorting bug
    filter:{name:searchText}

--- js/app.js
+++ js/app.js
@@ -5,8 +5,8 @@
 	var directoryInfo =
 	[
 		{
-			image: 		'img/Casie.png',
-			name: 		'Casie Lancaster',
+			image: 		"img/Casie.png",
+			name: 		"Casie Lancaster",
 			life: 		112,
 			strength:	64,
 			agility:	98,
@@ -14,16 +14,63 @@
 			wisdom:		188
 		},
 		{
-			image: 		'img/Mors.png',
-			name: 		'M',
+			name: 		"Mors Westford",
+			image: 		"img/Mors.png",
+			
 			life: 		248,
 			strength:	124,
 			agility:	56,
 			stamina:	187,
 			wisdom:		42
-		}
+		},
 		
+		{
+			image: 		"img/Arrow.png",
+			name: 		"Arrow",
+			life: 		312,
+			strength:	154,
+			agility:	200,
+			stamina:	288,
+			wisdom:		45
+		},
+		{
+			image: 		'img/BronzeKnife.png',
+			name: 		'Bronze Knife',
+			life: 		'',
+			strength:	'+5',
+			agility:	'',
+			stamina:	'',
+			wisdom:		''
+		},
+		{
+			image: 		'img/OakStaff.png',
+			name: 		'Oak Staff',
+			life: 		'',
+			strength:	'',
+			agility:	'',
+			stamina:	'',
+			wisdom:		'+ 10'
+		},
+		{
+			image: 		'img/StoneAxe.png',
+			name: 		'Stone Axe',
+			life: 		'',
+			strength:	'+35',
+			agility:	'',
+			stamina:	'',
+			wisdom:		''
+		},
+		{
+			image: 		'img/HunterBow.png',
+			name: 		"Hunter's Bow",
+			life: 		'',
+			strength:	'+15',
+			agility:	'+20',
+			stamina:	'',
+			wisdom:		''
+		}
 	];
+	
 	
 	app.service('profileService', function()
 	{
@@ -51,6 +98,16 @@
 	
 	app.controller('SearchController', function($scope, profileService)
 	{
+		
+		$scope.temp = [];
+		for(var i = 0; i < directoryInfo.length; i++)
+		{
+			$scope.temp[i] = 
+			{ 
+				name: directoryInfo[i].name
+			};
+		}
+		
 		$scope.directory = directoryInfo;
 		$scope.searchText = "";
 		$scope.numberOfResults = $('#sel_filter').val();
